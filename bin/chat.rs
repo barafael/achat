d35351f239f293324f04de5e873e1b4e9a7b547d
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-04-19 22:35:57 +0200

    Add tokio console to chat module

--- bin/chat.rs
+++ bin/chat.rs
@@ -1,9 +1,15 @@
 use achat::chat;
 use anyhow::Context;
+use std::time::Duration;
 use tokio::{net::TcpListener, sync::broadcast};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    console_subscriber::ConsoleLayer::builder()
+        .retention(Duration::from_secs(60))
+        .server_addr(([127, 0, 0, 1], 6669))
+        .init();
+
     let listener = TcpListener::bind("localhost:8080")
         .await
         .context("Failed to bind on localhost")?;
