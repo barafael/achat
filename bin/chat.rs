6cd32aaa2b79c4e5ff16b581ce35a76060929f75
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-04-27 11:38:04 +0000

    Add CLI args for chat bin

--- bin/chat.rs
+++ bin/chat.rs
@@ -1,16 +1,37 @@
 use achat::chat;
 use anyhow::Context;
+use clap::Parser;
+use std::net::SocketAddr;
 use std::time::Duration;
 use tokio::{net::TcpListener, sync::broadcast};
 
+/// Command Line Arguments
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Adress to listen on
+    #[clap(short, long)]
+    address: String,
+
+    /// Address to publish console events on
+    #[clap(short, long)]
+    console: Option<String>,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    console_subscriber::ConsoleLayer::builder()
-        .retention(Duration::from_secs(60))
-        .server_addr(([127, 0, 0, 1], 6669))
-        .init();
+    let args = Args::parse();
+    dbg!(&args);
 
-    let listener = TcpListener::bind("localhost:8080")
+    if let Some(console) = args.console {
+        let console: SocketAddr = console.parse().unwrap();
+        console_subscriber::ConsoleLayer::builder()
+            .retention(Duration::from_secs(60))
+            .server_addr(console)
+            .init();
+    }
+
+    let listener = TcpListener::bind(args.address)
         .await
         .context("Failed to bind on localhost")?;
 
