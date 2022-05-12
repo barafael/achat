5be744abc1126e6f49cb4b8b7cf2308780283313
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-05-12 23:42:19 +0200

    Remove CancellationToken from non-cancel example

--- bin/chat.rs
+++ bin/chat.rs
@@ -1,8 +1,7 @@
 use achat::{chat, init_console_subscriber, Args};
-use anyhow::{Context, Ok};
+use anyhow::Context;
 use clap::Parser;
 use tokio::{net::TcpListener, sync::broadcast};
-use tokio_util::sync::CancellationToken;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -18,37 +17,15 @@ async fn main() -> anyhow::Result<()> {
 
     let (tx, _rx) = broadcast::channel(16);
 
-    let mut handles = Vec::new();
-
-    let token = CancellationToken::new();
     loop {
-        let token = token.clone();
-        tokio::select! {
-            _ = token.cancelled() => {
-                break Ok(());
-            },
-            listen = listener.accept() => {
-                let (mut socket, addr) = listen.context("Failed to accept on socket")?;
-                let tx = tx.clone();
-                let rx = tx.subscribe();
+        if let Ok((mut socket, addr)) = listener.accept().await {
+            let tx = tx.clone();
+            let rx = tx.subscribe();
 
-                let h = tokio::spawn(async move {
-                    let (reader, writer) = socket.split();
-                    chat::handle_connection(addr, reader, writer, tx, rx)
-                        .await
-                        .context("Failed to handle connection")?;
-                    Ok(())
-                });
-                handles.push(h);
-            }
-        };
+            tokio::spawn(async move {
+                let (reader, writer) = socket.split();
+                chat::handle_connection(addr, reader, writer, tx, rx).await
+            });
+        }
     }
-    .context("Failed to run the accept loop")?;
-
-    futures::future::try_join_all(handles)
-        .await
-        .context("Unable to join client tasks")?
-        .into_iter()
-        .collect::<Result<(), _>>()
-        .context("Error in client task")
 }
